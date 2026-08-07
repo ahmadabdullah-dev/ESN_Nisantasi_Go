bdfdcc5014e71f0c9df1bc1eb5051ff842c223fb
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-08-08 01:17:39 +0300

    feat(web): Add theme.ts

--- WEB/src/main.tsx
+++ WEB/src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { theme } from './lib/theme.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}> 
+         <App />
+      <CssBaseline />
+    </ThemeProvider>
   </StrictMode>,
 )
