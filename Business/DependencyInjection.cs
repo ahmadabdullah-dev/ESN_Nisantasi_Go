db30c9b8c93608dd8b2f671b5492f2e8a3f38421
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-08-07 11:51:28 +0300

    Add EmailService

---
+++ Business/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Business;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBusiness(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddScoped<IEmailService, EmailService>();
+
+        services.Configure<EmailConfiguration>(configuration.GetSection("EmailConfiguration"));
+
+        return services;
+    }
+}
+
---
+++ Business/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using DataAccess.Entities;
+global using Business.Common.Configurations;
+global using Business.Interfaces;
+global using Business.Services;
